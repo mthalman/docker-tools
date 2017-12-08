cbc2cadc871269dd002523a1831e6a5efedd4db0
MichaelSimons <msimons@microsoft.com>
2017-12-07 22:33:32 -0600

    wip

--- Microsoft.DotNet.ImageDigestMonitor/image-digest-monitor.ps1
+++ Microsoft.DotNet.ImageDigestMonitor/image-digest-monitor.ps1
@@ -1,12 +1,12 @@
 param(
     [string]$Subscriptions = 'subscriptions.json',
     [string]$ImageBuilderImageName = 'microsoft/dotnet-buildtools-prereqs:image-builder-jessie-20171031123612',
-    [string]$VersionsGitUserName,
-    [string]$VersionsGitEmail,
-    [string]$VersionsGitPassword,
+    [string]$VersionsGitUserName = 'dotnet-build-bot',
+    [string]$VersionsGitEmail = 'dotnet-build-bot@microsoft.com',
+    [string]$VersionsGitAccessToken,
     [string]$VersionsGitBranch = 'master',
-    [string]$VersionsGitOwner,
-    [string]$Architecture
+    [string]$VersionsGitOwner = 'dotnet',
+    [string]$Architecture = 'amd64'
 )
 
 $ErrorActionPreference = 'Stop'
@@ -28,19 +28,38 @@ exec docker pull $ImageBuilderImageName
 
 $subscriptionsJson = Get-Content $Subscriptions | ConvertFrom-Json
 
-$repoSandbox = "./RepoSandbox";
+$repoSandbox = "$PSScriptRoot/RepoSandbox";
 New-Item -ItemType Directory -Path $repoSandbox
 Try {
     Push-Location $repoSandbox
     Try {
         $subscriptionsJson.Repos.PSObject.Properties |
             ForEach-Object {
-            $repoUrl = $_.Name
-            exec git clone $repoUrl
-            # $_.Value.Branches | ForEach-Object {
-            #     Write-Host "Cloning $repoUrl $_"
-            #     exec git clone $repoUrl $_
-            # }
+            $repoName = $_.Name
+            exec git clone $_.Value.Url $repoName
+
+            Push-Location $repoName
+            try {
+                $_.Value.Branches | ForEach-Object {
+                    $branch = $_
+                    exec git checkout $branch
+                    exec docker run --rm `
+                        -v /var/run/docker.sock:/var/run/docker.sock `
+                        -v "${repoSandbox}/${repoName}:/repo" `
+                        -w /repo `
+                        $ImageBuilderImageName `
+                        updateVersions `
+                            $VersionsGitUserName `
+                            $VersionsGitEmail `
+                            $VersionsGitAccessToken `
+                            --git-branch $VersionsGitBranch `
+                            --git-owner $VersionsGitOwner `
+                            --architecture $Architecture
+                }
+            }
+            Finally {
+                Pop-Location
+            }
         }
     }
     Finally {
@@ -50,10 +69,3 @@ Try {
 Finally {
     Remove-Item -Force -Recurse $repoSandbox
 }
-
-# & docker run --rm `
-#     -v /var/run/docker.sock:/var/run/docker.sock `
-#     -v "${repoRoot}:/repo" `
-#     -w /repo `
-#     $ImageBuilderImageName `
-#     generateTagsReadme "https://github.com/dotnet/${RepoName}/blob/${Branch}"
