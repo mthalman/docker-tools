8701d822077b097d0e04f400325dbd004a844319
Matt Thalman <mthalman@microsoft.com>
2020-03-20 08:27:00 -0500

    Add Created date

--- src/Microsoft.DotNet.ImageBuilder/src/Commands/ConvertImageInfoCommand.cs
+++ src/Microsoft.DotNet.ImageBuilder/src/Commands/ConvertImageInfoCommand.cs
@@ -5,7 +5,6 @@ using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-using Microsoft.DotNet.ImageBuilder.Models.Manifest;
 using Microsoft.DotNet.ImageBuilder.ViewModel;
 using Newtonsoft.Json;
 
@@ -17,11 +16,72 @@ namespace Microsoft.DotNet.ImageBuilder.Commands
     [Export(typeof(ICommand))]
     public class ConvertImageInfoCommand : ManifestCommand<ConvertImageInfoOptions>
     {
+        private readonly IDockerService dockerService;
+
+        [ImportingConstructor]
+        public ConvertImageInfoCommand(IDockerService dockerService)
+        {
+            this.dockerService = dockerService;
+        }
+
         public override Task ExecuteAsync()
         {
             string imageInfoText = File.ReadAllText(Options.ImageInfoPath);
-            V1.RepoData[] repos = JsonConvert.DeserializeObject<V1.RepoData[]>(imageInfoText);
 
+            V2.ImageArtifactDetails imageArtifactDetails;
+            if (Options.CreatedDate)
+            {
+                imageArtifactDetails = ImageInfoHelper.LoadFromContent(imageInfoText, Manifest);
+                GetCreatedDates(imageArtifactDetails);
+            }
+            else
+            {
+                V1.RepoData[] repos = JsonConvert.DeserializeObject<V1.RepoData[]>(imageInfoText);
+                imageArtifactDetails = GenerateDefault(repos);
+            }
+
+            string outputDir = Path.GetDirectoryName(Options.ImageInfoOutputPath);
+            string filenameBase = Path.GetFileNameWithoutExtension(Options.ImageInfoOutputPath);
+
+            string output = JsonHelper.SerializeObject(imageArtifactDetails);
+            File.WriteAllText(Options.ImageInfoOutputPath, output);
+
+            return Task.CompletedTask;
+        }
+
+        private void GetCreatedDates(V2.ImageArtifactDetails imageArtifactDetails)
+        {
+            foreach (var image in imageArtifactDetails.Repos.SelectMany(repo => repo.Images).Where(image => Manifest.GetFilteredImages().Contains(image.ManifestImage)))
+            {
+                DateTime mostRecentSimpleTag = DateTime.MinValue.ToUniversalTime();
+                if (image.SharedTags != null)
+                {
+                    mostRecentSimpleTag = image.SharedTags.First().Created;
+                }
+
+                foreach (var platform in image.Platforms)
+                {
+                    var platformInfo = image.ManifestImage.FilteredPlatforms.FirstOrDefault(p => platform.Equals(p));
+                    if (platformInfo != null)
+                    {
+                        this.dockerService.PullImage(platformInfo.Tags.First().FullyQualifiedName, false);
+                        platform.Created = this.dockerService.GetCreatedDate(platformInfo.Tags.First().FullyQualifiedName, false).ToUniversalTime();
+                        if (platform.Created > mostRecentSimpleTag)
+                        {
+                            mostRecentSimpleTag = platform.Created;
+                        }
+                    }
+                }
+
+                if (image.SharedTags != null)
+                {
+                    image.SharedTags.ForEach(tag => tag.Created = mostRecentSimpleTag);
+                }
+            }
+        }
+
+        private V2.ImageArtifactDetails GenerateDefault(V1.RepoData[] repos)
+        {
             V2.ImageArtifactDetails imageArtifactDetails = new V2.ImageArtifactDetails();
 
             foreach (var repo in repos)
@@ -83,7 +143,7 @@ namespace Microsoft.DotNet.ImageBuilder.Commands
                                         Digest = image.Value.Digest,
                                         OsType = manifestPlatform.Model.OS.ToString(),
                                         SimpleTags = image.Value.SimpleTags,
-                                        OsVersion = manifestPlatform.Model.OsVersion
+                                        OsVersion = manifestPlatform.GetOSDisplayName()
                                     };
                                 })
                                 .ToList()
@@ -93,13 +153,8 @@ namespace Microsoft.DotNet.ImageBuilder.Commands
                     }
                 }
             }
-            string outputDir = Path.GetDirectoryName(Options.ImageInfoPath);
-            string filenameBase = Path.GetFileNameWithoutExtension(Options.ImageInfoPath);
 
-            string output = JsonHelper.SerializeObject(imageArtifactDetails);
-            File.WriteAllText(Path.Combine(outputDir, $"{filenameBase}-converted.json"), output);
-
-            return Task.CompletedTask;
+            return imageArtifactDetails;
         }
 
         private static string GetPlatformDockerfile(PlatformInfo platform)
--- src/Microsoft.DotNet.ImageBuilder/src/Commands/ConvertImageInfoOptions.cs
+++ src/Microsoft.DotNet.ImageBuilder/src/Commands/ConvertImageInfoOptions.cs
@@ -5,22 +5,42 @@ using System.Text;
 
 namespace Microsoft.DotNet.ImageBuilder.Commands
 {
-    public class ConvertImageInfoOptions : ManifestOptions
+    public class ConvertImageInfoOptions : ManifestOptions, IFilterableOptions
     {
+        public ManifestFilterOptions FilterOptions { get; } = new ManifestFilterOptions();
+
         protected override string CommandHelp => "";
 
         public string ImageInfoPath { get; set; }
+
+        public string ImageInfoOutputPath { get; set; }
+
+        public bool CreatedDate { get; set; }
+
+        public override void DefineOptions(ArgumentSyntax syntax)
+        {
+            base.DefineOptions(syntax);
+            FilterOptions.DefineOptions(syntax);
+
+            bool createdDate = false;
+            syntax.DefineOption("created-date", ref createdDate, "");
+            CreatedDate = createdDate;
+        }
 
         public override void DefineParameters(ArgumentSyntax syntax)
         {
             base.DefineParameters(syntax);
 
             string imageInfoPath = null;
-            syntax.DefineParameter(
-                "image-info-path",
-                ref imageInfoPath,
-                "Image info file path");
+            syntax.DefineParameter("image-info-path", ref imageInfoPath, "");
             ImageInfoPath = imageInfoPath;
+
+            string imageInfoOutputPath = null;
+            syntax.DefineParameter(
+                "image-info-output-path",
+                ref imageInfoOutputPath,
+                "Image info file path");
+            ImageInfoOutputPath = imageInfoOutputPath;
         }
     }
 }
